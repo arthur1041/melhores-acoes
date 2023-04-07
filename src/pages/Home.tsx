f9de8d29a3a73680358246f9dcce25144a9d9f09
Arthur Cruz <arthur.cruz@esig.com.br>
2023-04-07 19:23:10 -0300

    salvando alteracoes do dia

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
 import { Box, Typography } from '@mui/material';
+import axios, { AxiosRequestConfig } from 'axios';
+import EnvVariables from '../utils/variables.utils';
+import { useEffect } from 'react';
 
 export const Home = () => {
+  const axiosConfig: AxiosRequestConfig = {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
+  const getStocksList = async () => {
+    const responseData = (
+      await axios.get(`${EnvVariables.INVEST_SITE_BASE_URL}`, axiosConfig)
+    ).data;
+
+    console.log('reposta investsite', responseData);
+
+    // axios.post('');
+  };
+
+  useEffect(() => {
+    getStocksList();
+  });
+
   return (
     <Box>
-      <Typography>Ready to go!</Typography>
+      <Typography>Ready to go! 123</Typography>
     </Box>
   );
 };
---
+++ src/utils/variables.utils.ts
@@ -0,0 +1,6 @@
+class EnvVariables {
+  static INVEST_SITE_BASE_URL = 'https://www.investsite.com.br';
+  static PROXY_ANYWHERE_URL = 'https://cors-anywhere.herokuapp.com';
+}
+
+export default EnvVariables;
